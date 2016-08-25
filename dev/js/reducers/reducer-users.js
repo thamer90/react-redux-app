8c9f650ed9dd240e503fecef5543b91deedc8d07
Thamer Al-Merry <Thamer.AlMerry@webjet.com.au>
2016-08-25 16:37:49 +1000

    Added reducers, containers, components and actions

--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -3,31 +3,31 @@
  * You need to return something, so if there are no users then just return an empty array
  * */
 
-export default function () {
-    return [
-        {
-            id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
-            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
-        },
-        {
-            id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
-            thumbnail: "http://i.imgur.com/52xRlm8.png"
-        },
-        {
-            id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
-            thumbnail: "http://i.imgur.com/4EMtxHB.png"
-        }
-    ]
-}
+ export default function() {
+     return [
+         {
+             id: 1,
+             first: "Ulquiorra",
+             last: "Schiffer",
+             age: 26,
+             description: "Ulquiorra Cifer (ウルキオラ・シファー, Urukiora Shifā)[2] is the Cuatro (4th) Espada in Aizen's army of Arrancar.",
+             thumbnail: "http://vignette1.wikia.nocookie.net/bleach/images/f/f7/267Ulquiorra_profile.png/revision/latest/scale-to-width-down/250?cb=20140605165650&path-prefix=en"
+         },
+         {
+             id: 2,
+             first: "Aizen",
+             last: "Sosuke",
+             age: 31,
+             description: "Sōsuke Aizen (藍染 惣右介, Aizen Sōsuke) is the former captain of the 5th Division in the Gotei 13.",
+             thumbnail: "http://vignette4.wikia.nocookie.net/bleach/images/a/ab/Aizen.png/revision/latest/scale-to-width-down/250?cb=20140321023221&path-prefix=en"
+         },
+         {
+             id: 3,
+             first: "Askin",
+             last: "Nakk Le Vaar",
+             age: 72,
+             description: "Askin Nakk Le Vaar (アスキン・ナックルヴァール, Asukin Nakkuruvāru) is a Quincy and a member of the Wandenreich's Sternritter with the designation D - The Deathdealing.",
+             thumbnail: "http://vignette1.wikia.nocookie.net/vsbattles/images/3/3f/Askin-0.png/revision/latest?cb=20151226150513"
+         }
+     ]
+ }
